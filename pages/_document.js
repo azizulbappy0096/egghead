fcd1be21f11e91c1afd00c7e83fc771d05d5ca5f
Azizul Bappy <azizulbappy6@gmail.com>
2021-05-28 19:26:32 +0600

    add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,31 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta
+            name="description"
+            content="This is a clone page of egghead.io"
+          />
+
+          <meta name="keywords" content="egghead, egghead.io, azizul' dev" />
+          <meta name="author" content="Azizul Bappy" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <body className="normal-mode">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
